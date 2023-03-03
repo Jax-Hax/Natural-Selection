1465c35697c04071abe9236af0bb5de562dde0b1
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2023-03-03 14:34:42 -0500

    about to change to a svelte file

--- src/routes/simulation/play/+page.svelte
+++ src/routes/simulation/play/+page.svelte
@@ -8,6 +8,7 @@
 	onMount(() => {
 		startGame(el);
 	});
+	
 </script>
 
 <body>
--- src/routes/simulation/play/scene.js
+++ src/routes/simulation/play/scene.js
@@ -1,39 +1,50 @@
 import * as BABYLON from '@babylonjs/core/Legacy/legacy';
 import { sizeX, sizeY, generations, amMice, amSnakes, amCats } from '$lib/stores.js';
-var engine,scene,camera,light,ground,mouse,cat,snake;
+var engine, scene, camera, light, ground, mouse, cat, snake;
 const mice = [];
 const cats = [];
 const snakes = [];
-function createScene(canvas){
-	console.log(sizeX);
-    engine = new BABYLON.Engine(canvas);
+var sizeY_;
+sizeY.subscribe((y) => {
+	sizeY_ = sizeY;
+});
+function createScene(canvas) {
+	engine = new BABYLON.Engine(canvas);
 	scene = new BABYLON.Scene(engine);
 	scene.debugLayer.show();
 	scene.clearColor = new BABYLON.Color3(0, 50, 25);
-	camera = new BABYLON.UniversalCamera("camera1", new BABYLON.Vector3(0, 1, -sizeY), scene);
-    camera.attachControl(canvas, true);
-    camera.checkCollisions = true;
-    camera.speed = 1;
+	camera = new BABYLON.UniversalCamera(
+		'camera1',
+		new BABYLON.Vector3(0, 1, -parseInt(sizeY_)),
+		scene
+	);
+	camera.attachControl(canvas, true);
+	camera.checkCollisions = true;
+	camera.speed = 1;
 	camera.keysUp.push(87); // “w”
-camera.keysDown.push(83); // “s”
-camera.keysLeft.push(65); // “a”
-camera.keysRight.push(68); // “d”
-    camera.inputs.addMouseWheel();
-	light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, -15), scene);
-  	light.intensity = 0.5;
-  	ground = BABYLON.MeshBuilder.CreateGround("ground", {height: 100/*sizeX*/, width: 400/*sizeY*/, subdivisions: 1});
-	var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
+	camera.keysDown.push(83); // “s”
+	camera.keysLeft.push(65); // “a”
+	camera.keysRight.push(68); // “d”
+	camera.inputs.addMouseWheel();
+	light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, -15), scene);
+	light.intensity = 0.5;
+	ground = BABYLON.MeshBuilder.CreateGround('ground', {
+		height: 100 /*sizeX*/,
+		width: 400 /*sizeY*/,
+		subdivisions: 1
+	});
+	var groundMaterial = new BABYLON.StandardMaterial('groundMaterial', scene);
 	groundMaterial.backFaceCulling = false;
 	ground.material = groundMaterial;
 }
-function initFirstGen(){
-	for (let i = 0; i < 5/*amMice.length*/; i++) {
-		let mouse = new Mouse(10,10,1);
+function initFirstGen() {
+	for (let i = 0; i < 5 /*amMice.length*/; i++) {
+		let mouse = new Mouse(10, 10, 1);
 	}
 }
-function gameLoop(canvas){
+function gameLoop(canvas) {
 	createScene(canvas);
-  	initFirstGen();
+	initFirstGen();
 	/*var box = BABYLON.Mesh.CreateBox('box', 2, scene);
 	box.rotation.x = -0.2;
 	box.rotation.y = -0.4;
@@ -66,23 +77,21 @@ function gameLoop(canvas){
 		torus.scaling.z = Math.abs(Math.sin(t * 2)) + 0.5;
 		cylinder.position.y = Math.sin(t * 3);*/
 	};
-  var initScene = function () {
-    
-  };
+	var initScene = function () {};
 	engine.runRenderLoop(renderLoop);
 	return scene;
 }
 const startGame = (canvas) => {
 	gameLoop(canvas);
 };
-export {startGame};
+export { startGame };
 class Mouse {
-    constructor(posX, posY, generation) {
-      this.posX = posX;
-      this.posY = posY;
-      this.generation = generation;
-    }
+	constructor(posX, posY, generation) {
+		this.posX = posX;
+		this.posY = posY;
+		this.generation = generation;
+	}
 	posX;
 	posY;
 	generation;
-  }
+}
