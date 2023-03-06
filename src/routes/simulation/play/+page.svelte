5b9326d14887adce7de8ac1b96bcc79a511da7b1
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2023-03-06 13:16:24 -0500

    omg somehow it works now??

--- src/routes/simulation/play/+page.svelte
+++ src/routes/simulation/play/+page.svelte
@@ -1,14 +1,54 @@
 <script>
 	import { onMount } from 'svelte';
-	import { startGame } from './scene.js';
-	/**
-	 * @type {HTMLCanvasElement}
-	 */
 	let el;
 	onMount(() => {
 		startGame(el);
 	});
-	
+	import * as BABYLON from '@babylonjs/core/Legacy/legacy';
+import { sizeX, sizeY, generations, amMice, amSnakes, amCats } from '$lib/stores.js';
+var engine, scene, camera, light, ground, mouse, cat, snake;
+const mice = [];
+const cats = [];
+const snakes = [];
+function createScene(canvas) {
+	engine = new BABYLON.Engine(canvas);
+	scene = new BABYLON.Scene(engine);
+	scene.clearColor = new BABYLON.Color3(0, 50, 25);
+	camera = new BABYLON.UniversalCamera(
+		'camera1',
+		new BABYLON.Vector3(0, 1, -$sizeY),
+		scene
+	);
+	camera.attachControl(canvas, true);
+	camera.checkCollisions = true;
+	camera.speed = 1;
+	camera.keysUp.push(87); // “w”
+	camera.keysDown.push(83); // “s”
+	camera.keysLeft.push(65); // “a”
+	camera.keysRight.push(68); // “d”
+	camera.inputs.addMouseWheel();
+	light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, -15), scene);
+	light.intensity = 0.5;
+	ground = BABYLON.MeshBuilder.CreateGround('ground', {
+		height: 100 /*sizeX*/,
+		width: 400 /*sizeY*/,
+		subdivisions: 1
+	});
+	var groundMaterial = new BABYLON.StandardMaterial('groundMaterial', scene);
+	groundMaterial.backFaceCulling = false;
+	ground.material = groundMaterial;
+}
+function gameLoop(canvas) {
+	createScene(canvas);
+	var renderLoop = function () {
+		scene.render();
+	};
+	engine.runRenderLoop(renderLoop);
+	return scene;
+}
+const startGame = (canvas) => {
+	gameLoop(canvas);
+};
 </script>
 
 <body>
