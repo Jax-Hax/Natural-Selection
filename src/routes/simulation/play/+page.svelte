0053be5c987703070cfa65e98462dfeaaf197e3c
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2023-03-07 11:30:12 -0500

    fixed camera

--- src/routes/simulation/play/+page.svelte
+++ src/routes/simulation/play/+page.svelte
@@ -6,6 +6,7 @@
 	});
 	import * as BABYLON from '@babylonjs/core/Legacy/legacy';
 import { sizeX, sizeY, generations, amMice, amSnakes, amCats } from '$lib/stores.js';
+//const sizeX = 100, sizeY = 100, amMice = 5;
 var engine, scene, camera, light, ground, mouse, cat, snake;
 const mice = [];
 const cats = [];
@@ -13,10 +14,10 @@ const snakes = [];
 function createScene(canvas) {
 	engine = new BABYLON.Engine(canvas);
 	scene = new BABYLON.Scene(engine);
-	scene.clearColor = new BABYLON.Color3(0, 50, 25);
+	scene.clearColor = new BABYLON.Color3(0, 50, 75);
 	camera = new BABYLON.UniversalCamera(
 		'camera1',
-		new BABYLON.Vector3(0, 1, -$sizeY),
+		new BABYLON.Vector3(0, 15, -$sizeY),
 		scene
 	);
 	camera.attachControl(canvas, true);
@@ -30,20 +31,31 @@ function createScene(canvas) {
 	light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, -15), scene);
 	light.intensity = 0.5;
 	ground = BABYLON.MeshBuilder.CreateGround('ground', {
-		height: 100 /*sizeX*/,
-		width: 400 /*sizeY*/,
+		height: $sizeX,
+		width: $sizeY,
 		subdivisions: 1
 	});
 	var groundMaterial = new BABYLON.StandardMaterial('groundMaterial', scene);
 	groundMaterial.backFaceCulling = false;
 	ground.material = groundMaterial;
 }
+function makeFirstGeneration(){
+	for (let i = 0; i < amMice.length; i++) {
+		let mouse = new Mouse(10, 10, 1);
+		const mouse2 = BABYLON.MeshBuilder.CreateBox("box", {width: 2, height: 1.5, depth: 3})
+	}
+}
 function gameLoop(canvas) {
 	createScene(canvas);
+	makeFirstGeneration();
 	var renderLoop = function () {
 		scene.render();
 	};
 	engine.runRenderLoop(renderLoop);
+	
+window.addEventListener("resize", function () {
+        engine.resize();
+      });
 	return scene;
 }
 const startGame = (canvas) => {
